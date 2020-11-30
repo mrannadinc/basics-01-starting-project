4942e19a5da77444f7e3c60b478b59c16576fe4d
Halil İbrahim <h.ibrahim2321@gmail.com>
2020-11-30 17:13:28 +0300

    commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,7 +4,5 @@ let currentResult = defaultResult;
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 -1`;
-let errorMessege = 'An error \'' +
-                   'occurred!'
 
 outputResult(currentResult, errorMessege);
