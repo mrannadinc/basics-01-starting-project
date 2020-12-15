4e19b92700c27dabd7f50a7b5b1855416cbba560
Halil İbrahim <h.ibrahim2321@gmail.com>
2020-12-15 10:02:52 +0300

    commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -65,23 +65,23 @@ function calculateResult(calculationType){
   //}
 }
 
-function add() {
-  calculateResult('ADD');
-}
+// function add() {
+//   calculateResult('ADD');
+// }
 
-function subtract() {
-  calculateResult('SUBTRACT');
-}
+// function subtract() {
+//   calculateResult('SUBTRACT');
+// }
 
-function multiply() {
-  calculateResult('MULTIPLY');
-}
+// function multiply() {
+//   calculateResult('MULTIPLY');
+// }
 
-function divide() {
-  calculateResult('DIVIDE');
-}
+// function divide() {
+//   calculateResult('DIVIDE');
+// }
 
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', subtract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
+addBtn.addEventListener('click', calculateResult.bind(this, 'ADD'));
+subtractBtn.addEventListener('click', calculateResult.bind(this, 'SUBTRACT'));
+multiplyBtn.addEventListener('click', calculateResult.bind(this, 'MULTIPLY'));
+divideBtn.addEventListener('click', calculateResult.bind(this, 'DIVIDE'));
